c7189edf69de0860f6ef1666cab2a2d21e0c4a46
Artem <artem.makanov@mail.ru>
2026-06-09 12:48:09 +0300

    [step_0] test task

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,10 +1,16 @@
 from clearml import Task
 
 # 1. Инициализируем задачу
-# 'students_project' — название проекта, 'test_task' — имя эксперимента
 task = Task.init(project_name='students_project', task_name='test_task_on_agent')
 
-# 2. Указываем, что эту задачу нужно выполнить удаленно в очереди 'students'
+# 2. Указываем конкретную ветку
+# ClearML автоматически найдет репозиторий, если скрипт в нем лежит
+task.set_repo(
+    repo='https://github.com/Artemmakanov/aith_mlops.git',
+    branch='step_0',
+)
+
+# 3. Отправляем в очередь
 task.execute_remotely(queue_name='students')
 
 # --- Код, который будет выполняться на агенте ---
